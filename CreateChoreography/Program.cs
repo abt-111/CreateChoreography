16912f7985d0f405b4ec07ceb9945a06ae44aaa3
abt93 <anthony.temmel@gmail.com>
2024-06-06 10:20:33 +0200

    Ajout de la saisie de l'utilisateur

--- CreateChoreography/Program.cs
+++ CreateChoreography/Program.cs
@@ -4,6 +4,19 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        int i = 0; // Pour parcourir moves dans la boucle while
+        string userInput = "";
+        string[] moves = new string[32];
 
+        // Saisie de l'utilisateur
+
+        Console.WriteLine("Crée ta chorégraphie (" + moves.Length + " pas max)\n");
+
+        while(userInput != "done" && i < moves.Length)
+        {
+            Console.WriteLine("Entrez un pas de dance (entrez \"done\" pour terminer)\n");
+            userInput = Console.ReadLine();
+            i++;
+        }
     }
 }
