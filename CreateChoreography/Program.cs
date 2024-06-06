731f418872886273dbb5656393aad2318610668a
abt93 <anthony.temmel@gmail.com>
2024-06-06 10:36:24 +0200

    Ajout de la saisie du nombre de répétition

--- CreateChoreography/Program.cs
+++ CreateChoreography/Program.cs
@@ -5,6 +5,7 @@ class MainClass
     public static void Main(string[] args)
     {
         int i = 0; // Pour parcourir moves dans la boucle while
+        int repeatNumber = 1;
         string userInput = "";
         string[] moves = new string[32];
 
@@ -14,9 +15,17 @@ class MainClass
 
         while(userInput != "done" && i < moves.Length)
         {
-            Console.WriteLine("Entrez un pas de dance (entrez \"done\" pour terminer)\n");
+            Console.WriteLine("Entrez un pas de dance (entrez \"done\" pour terminer)");
             userInput = Console.ReadLine();
+
+            if(userInput != "done")
+            {
+                moves[i] = userInput;
+            }
             i++;
         }
+
+        Console.WriteLine("Entrez le nombre de fois que les pas doivent être exécutés");
+        repeatNumber = Convert.ToInt32(Console.ReadLine());
     }
 }
