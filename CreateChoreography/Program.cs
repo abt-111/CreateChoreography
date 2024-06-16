6d01e18e609155e81fc31c11c4f4a516f203e936
abt-111 <anthony.temmel@gmail.com>
2024-06-16 20:34:34 +0200

    Ajout du contrôle de la valeur du nombre de répétition

--- CreateChoreography/Program.cs
+++ CreateChoreography/Program.cs
@@ -1,19 +1,33 @@
 ﻿class MainClass
 {
-    public const int MAX_MOVES = 32;
+    public const int MAX_MOVES = 2;
     public static void Main(string[] args)
     {
         int repeatNumber = 1;
+        string userInput;
         string choregraphy = "";
         string[] moves = new string[MAX_MOVES];
 
         Console.WriteLine("Crées ta chorégraphie (" + moves.Length + " pas max)\n");
 
+        // Saisie de pas de danse par l'utilisateur
         SetAllMoves(moves);
 
+        bool isValid = false;
+
         // Saisie du nombre de répétition
-        Console.Write("Entres le nombre de répétition des pas : ");
-        repeatNumber = Convert.ToInt32(Console.ReadLine());
+        while (!isValid)
+        {
+            Console.Write("Entres le nombre de répétition des pas : ");
+            userInput = Console.ReadLine();
+
+            isValid = int.TryParse(userInput, out repeatNumber);
+
+            if (!isValid)
+            {
+                Console.WriteLine($"Tu dois entrer un entier compris entre {Int32.MinValue} et {Int32.MaxValue} \n");
+            }
+        }
 
         // Affichage de la chorégraphie
         Console.WriteLine("\nTa chorégraphie");
@@ -30,7 +44,7 @@
         }
     }
 
-    public static bool ValidUserInput(string userInput)
+    public static bool ValidUserInputForMoves(string userInput)
     {
         // On vérifie si la valeur est vide ou composé uniquement de ' '
         if (userInput.Trim() != string.Empty)
@@ -64,11 +78,10 @@
         {
             Console.Write($"Entres le pas n°{index} (ou \"done\" pour terminer) : ");
 
-            // Saisie de l'utilisateur
             userInput = Console.ReadLine();
 
             // Vérification de la saisie de l'utilisateur
-            isValid = ValidUserInput(userInput);
+            isValid = ValidUserInputForMoves(userInput);
 
             // Si la saisie de l'utilisateur est non valide on ne fait rien d'autre
             if (!isValid) continue;
